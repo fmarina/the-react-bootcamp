1f4c0f3a010673589517a5622a8f5311e1dfbb3b
Marina <marinafaenze@hotmail.com>
2020-07-01 11:43:06 -0300

    Appcontext imported to App.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,13 @@ import './App.css';
 // import Example from './Practice/6-RenderProps/Example';
 // import AppRenderProps from './Practice/6-RenderProps/Example RenderProps/AppRenderProps';
 // import AppRenderProps from './Practice/6-RenderProps/RenderPropsPractice/AppRenderProps';
-import AppShouldComponentUpdate from './Practice/7-ShouldComponentUpdate-ReactMemo/AppComponent';
+// import AppShouldComponentUpdate from './Practice/7-ShouldComponentUpdate-ReactMemo/AppComponent';
+// import AppContext from './Practice/8-ContextProvider/AppContext';
+// import AppContext from './Practice/9-ContextProviderPractice/AppContext';
 
 function App() {
   return (
-      <div>
-        <AppShouldComponentUpdate />
+      <div>      
       </div>
   )
 }
