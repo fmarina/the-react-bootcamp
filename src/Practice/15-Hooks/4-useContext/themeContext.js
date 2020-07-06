3ecfdd746603519dd016379087df3aeb80b599bf
Marina <marinafaenze@hotmail.com>
2020-07-06 11:47:12 -0300

    before start challenge of Convert themecontextprovider class to functional Component

--- src/Practice/15-Hooks/4-useContext/themeContext.js
+++ src/Practice/15-Hooks/4-useContext/themeContext.js
@@ -1,5 +1,9 @@
 import React, {Component} from "react"
 const ThemeContext = React.createContext()
+/**
+ * Convert this ThemeContextProvider into a functional component that uses hooks
+ * Tip: Use the browser dev tools if you run into a bug that isn't clear from the Scrimba console logs
+ */
 
 class ThemeContextProvider extends Component {
     state = { 
