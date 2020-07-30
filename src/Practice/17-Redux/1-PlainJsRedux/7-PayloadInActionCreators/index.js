147dfd4c57fded812f822f10c1ac833346e6a050
Marina <marinafaenze@hotmail.com>
2020-07-30 17:24:57 -0300

    payload property added to action

---
+++ src/Practice/17-Redux/1-PlainJsRedux/7-PayloadInActionCreators/index.js
@@ -0,0 +1,27 @@
+const redux = require("redux")
+
+function changeCount(amount = 1) {
+    return {
+        type: "CHANGE_COUNT",
+        payload: amount
+    }
+}
+
+function reducer(state = {count: 0}, action) {
+    // return new state based on the incoming action.type
+    switch(action.type) {
+        case "CHANGE_COUNT":
+            return {
+                count: state.count + action.payload
+            }
+        default:
+            return state
+    }
+}
+
+const store = redux.createStore(reducer)
+store.subscribe(() => {
+    console.log(store.getState())
+})
+
+store.dispatch(changeCount(4))
