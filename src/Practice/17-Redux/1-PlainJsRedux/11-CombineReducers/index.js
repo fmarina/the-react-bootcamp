fb4aa0e3c422dde29558599fad33f2eae6076980
Marina <marinafaenze@hotmail.com>
2020-07-31 18:16:44 -0300

    practice 1

--- src/Practice/17-Redux/1-PlainJsRedux/11-CombineReducers/index.js
+++ src/Practice/17-Redux/1-PlainJsRedux/11-CombineReducers/index.js
@@ -1,4 +1,17 @@
 import store from './redux/index';
 import {changeCount} from './redux/count';
+import {addFavoriteThing, removeFavoriteThing} from './redux/favoriteThings';
+import {
+    setYouTubeTitle, 
+    incrementViewCount,
+    upvoteVideo,
+    downvoteVideo
+} from './redux/youtubeVideo';
 
 store.dispatch(changeCount(42));
+
+store.dispatch(addFavoriteThing("A book"));
+store.dispatch(addFavoriteThing("Walk with a dog"));
+store.dispatch(removeFavoriteThing("walk with a dog"));
+
+store.dispatch(setYouTubeTitle("The react bootcamp video"));
